db4fdbe522509cd7ef724ca7a0a87256c1429ee3
silenium-dev <me@silenium-dev.net>
2026-06-18 23:19:19 +0200

    build: configure licensing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import dev.silenium.gradle.conventions.publishing
+import dev.silenium.gradle.conventions.BaseExtension
 
 plugins {
     alias(libs.plugins.conventions.jvm)
@@ -9,32 +10,34 @@ allprojects {
     group = "dev.silenium.libs.jni"
 
     afterEvaluate {
-        conventions {
-            publishing {
-                pomSpec.set {
-                    name = project.name
-                    description = "A library for working with native libraries"
-                    url = "https://github.com/silenium-dev/jni-utils"
-                    inceptionYear = "2024"
-                    licenses {
-                        license {
-                            name = "GPL-3.0-or-later"
-                            url = "https://spdx.org/licenses/GPL-3.0-or-later.html"
-                        }
-                    }
-                    developers {
-                        developer {
-                            id = "silenium-dev"
-                            email = "support@silenium-dev.net"
-                        }
-                    }
-                    scm {
-                        connection = "scm:git:git://github.com/silenium-dev/jni-utils.git"
-                        developerConnection = "scm:git:ssh://github.com/silenium-dev/jni-utils.git"
+        extensions.findByName("conventions")
+            ?.let { it as BaseExtension }
+            ?.apply {
+                publishing {
+                    pomSpec.set {
+                        name = project.name
+                        description = "A library for working with native libraries"
                         url = "https://github.com/silenium-dev/jni-utils"
+                        inceptionYear = "2024"
+                        licenses {
+                            license {
+                                name = "GPL-3.0-or-later"
+                                url = "https://spdx.org/licenses/GPL-3.0-or-later.html"
+                            }
+                        }
+                        developers {
+                            developer {
+                                id = "silenium-dev"
+                                email = "support@silenium-dev.net"
+                            }
+                        }
+                        scm {
+                            connection = "scm:git:git://github.com/silenium-dev/jni-utils.git"
+                            developerConnection = "scm:git:ssh://github.com/silenium-dev/jni-utils.git"
+                            url = "https://github.com/silenium-dev/jni-utils"
+                        }
                     }
                 }
             }
-        }
     }
 }
